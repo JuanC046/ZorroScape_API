25d92a013df397052df8de2ac7ed8987d820c12d
juanc <juan.lopez15@utp.edu.co>
2023-07-15 23:09:17 -0500

    Create method updateFile

--- services/status.service.js
+++ services/status.service.js
@@ -7,10 +7,34 @@ class StatusService extends FileService {
 
   async getData(fileName, fileType){
     let data = await this.readFile(fileName, fileType);
-    data = JSON.parse(data);
     return data;
   }
 
+  async updateFile(fileName, fileType,newData) {
+    let actualData = await this.getData(fileName,fileType);
+    actualData = JSON.parse(actualData);
+    let data = {}
+    {
+      if (actualData.x > -10 && actualData.x < 10){
+        data["x"] = actualData.x += newData.x 
+      } else {
+        data["x"] = actualData.x
+      }
+      if (actualData.y > -10 && actualData.y < 10){
+        data["y"] = actualData.y += newData.y
+      }else{
+        data["y"] = actualData.y
+      }
+      if (actualData.attacking > -10 && actualData.attacking < 10){
+        data["attacking"] = actualData.attacking += newData.attacking
+      }else{
+        data["attacking"] = actualData.attacking
+      }
+        data["jumping"] = actualData.jumping = newData.jumping
+    }
+
+    await this.updateText(fileName, fileType,JSON.stringify(data));
+  }
 }
 
 
