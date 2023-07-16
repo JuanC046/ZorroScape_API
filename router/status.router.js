5a4c052c5b16eceb150665b67e10d6f9426b594b
juanc <juan.lopez15@utp.edu.co>
2023-07-15 23:10:38 -0500

    Create endpoint movement/right

--- router/status.router.js
+++ router/status.router.js
@@ -2,8 +2,22 @@ const router = require('express').Router();
 const statusService = require('../services/status.service');
 
 router.get('/idle/current', async (req, res, err) => {
-    const status = await statusService.getData("game_status","json");
+    let status = await statusService.getData("game_status","json");
+    status = JSON.parse(status);
     res.status(200).json(status);
   });
 
+  router.put('/movement/right', async (req, res, err) => {
+    newData = {
+      "x": 10,
+      "y": 0,
+      "attacking":0,
+      "jumping": false
+    }
+    await statusService.updateFile("game_status","json",newData);
+    let status = await statusService.getData("game_status","json");
+    status = JSON.parse(status);
+    res.status(200).json(status);
+  });
+  
 module.exports = router;
